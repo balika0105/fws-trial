f0e4d3f464ba0c0b8b00821ddaa9334c3923358b
Balázs M. <40917873+balika0105@users.noreply.github.com>
2024-10-04 15:14:58 +0200

    Final touches 1

--- htroot/app/view/home.php
+++ htroot/app/view/home.php
@@ -43,7 +43,14 @@
         <p>A második feladat megoldása egy MySQL lekérdezés, ami a következő:</p>
         <p>
             <code>
-            SELECT SUM(sub.price) AS package_sum FROM( SELECT `price_history`.`price` FROM price_history INNER JOIN `product_package_contents` ON `price_history`.`product_id` = `product_package_contents`.`product_id` WHERE `product_package_contents`.`product_package_id` = 2264 GROUP BY `product_package_contents`.`product_id` ORDER BY ABS(DATEDIFF (`price_history`.`updated_at`, 2023-12-01)) ASC) AS sub;
+            SELECT SUM(sub.smallsum) AS package_sum
+FROM(
+SELECT (`price_history`.`price` * `product_package_contents`.`quantity`) AS smallsum FROM price_history
+INNER JOIN `product_package_contents` ON `price_history`.`product_id` = `product_package_contents`.`product_id`
+WHERE `product_package_contents`.`product_package_id` = 2264
+GROUP BY `product_package_contents`.`product_id`
+ORDER BY ABS(DATEDIFF (`price_history`.`updated_at`, 2023-12-01)) ASC)
+AS sub;
             </code>
         </p>
         <p>
